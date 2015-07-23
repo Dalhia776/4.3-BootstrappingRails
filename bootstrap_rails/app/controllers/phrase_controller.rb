a42bb429cefde96edda4b19b69abf2a57b0ab76e
Dallie Hill <lenore776@gmail.com>
2015-07-23 12:04:12 -0400

    Quotes work now

--- bootstrap_rails/app/controllers/phrase_controller.rb
+++ bootstrap_rails/app/controllers/phrase_controller.rb
@@ -5,14 +5,13 @@ class PhraseController < ApplicationController
   end
 
   def show_quote
-    @quotes =  {"FERRIS BUELLER’S DAY OFF" => “Bueller….Bueller…Bueller?”,
-                "THE SHINING"=> “Heeeeere’s Johnny!”,
-                "TOMBSTONE" => “I’m your Huckleberry.”,
-                "SCARFACE" => “Say ‘hello’ to my little friend!”,
-                "THE GOONIES" => “Hey you guys!”,
-                "OFFICE SPACE" => “Sounds like SOMEBODY’S got a case of the Mondays!”}
-          render template: => 'phrase/show_quote.html.rb'
-  end
+    @quotes =  {"FERRIS BUELLER’S DAY OFF" => "Bueller….Bueller…Bueller?",
+                "THE SHINING"=> "Heeeeere’s Johnny!",
+                "TOMBSTONE" => "I’m your Huckleberry.",
+                "SCARFACE" => "Say ‘hello’ to my little friend!",
+                "THE GOONIES" => "Hey you guys!",
+                "OFFICE SPACE" => "Sounds like SOMEBODY’S got a case of the Mondays!"}
+    end
 
 
 
--- bootstrap_rails/config/routes.rb
+++ bootstrap_rails/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
     # get 'products/:id' => 'catalog#view'
     get 'say_hello/:name(/:last_name)' => 'welcome#say_hello', as: :namer
-    get 'show_quote/:quotes' => 'phrase#show_quote'
+    get 'show_quote/(:quotes)' => 'phrase#show_quote'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
