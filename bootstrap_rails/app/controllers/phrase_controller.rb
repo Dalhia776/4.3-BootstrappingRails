e170543f575ba518cd9f68d1899fc9801114b32e
Dallie Hill <lenore776@gmail.com>
2015-07-23 07:13:25 -0400

    My bootstrap rails project

---
+++ bootstrap_rails/app/controllers/phrase_controller.rb
@@ -0,0 +1,20 @@
+class PhraseController < ApplicationController
+
+  def index
+    render plain: "Here are some of my favorite movie quotes."
+  end
+
+  def show_quote
+    @quotes =  {"FERRIS BUELLER’S DAY OFF" => “Bueller….Bueller…Bueller?”,
+                "THE SHINING"=> “Heeeeere’s Johnny!”,
+                "TOMBSTONE" => “I’m your Huckleberry.”,
+                "SCARFACE" => “Say ‘hello’ to my little friend!”,
+                "THE GOONIES" => “Hey you guys!”,
+                "OFFICE SPACE" => “Sounds like SOMEBODY’S got a case of the Mondays!”}
+          render template: => 'phrase/show_quote.html.rb'
+  end
+
+
+
+
+end
